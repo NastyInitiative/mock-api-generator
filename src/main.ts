a38d23d5b730e6e47e454a843c54b7383ec8b534
NastyInitiative <rokk91@Rokk.localdomain>
2020-10-11 12:34:20 +0200

    fix: implemented base UI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {  NestExpressApplication  } from "@nestjs/platform-express";
+import { join } from "path";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  /* app.use('/scripts', app.useStaticAssets(join(__dirname, '..', 'node_modules/bootstrap/dist')))
+  app.useStaticAssets(join(__dirname, '..', 'node_modules/bootstrap/dist')) */
+  app.useStaticAssets(join(__dirname, '..', 'ui'));
+  
+  app.setBaseViewsDir(join(__dirname, '..', 'ui'));
+  app.setViewEngine('hbs');
+  
   app.enableCors();
   await app.listen(5000);
 }
